cbe6fd716d353b2a40417e481b5d8e19c52777ad
吴晓妍 <914120660@qq.com>
2021-06-08 21:10:44 +0800

    small problem

--- js/video.js
+++ js/video.js
@@ -1,71 +1,76 @@
-var video=document.getElementById("player1");
-var btnstart=document.getElementById("play");
-var btnpause=document.getElementById("pause");
-var btnslow=document.getElementById("slower");
-var btnfast=document.getElementById("faster");
-var btnskip=document.getElementById("skip");
-var btnmute=document.getElementById("mute");
+var video = document.getElementById("player1");
+var btnstart = document.getElementById("play");
+var btnpause = document.getElementById("pause");
+var btnslow = document.getElementById("slower");
+var btnfast = document.getElementById("faster");
+var btnskip = document.getElementById("skip");
+var btnmute = document.getElementById("mute");
+var slider = document.getElementById("slider");
+var volume = document.getElementById("volume");
 
-window.addEventListener("load", function() {
+video.autoplay = false
+video.loop = true
+video.load()
+
+window.addEventListener("load", function () {
 	console.log("Good job opening the window")
-
 });
 
-btnstart.addEventListener("click",function(){
+btnstart.addEventListener("click", function () {
 	video.play()
+	volume.innerText = slider.value + "%"
+	console.log("play video")
 })
 
-btnpause.addEventListener("click",function(){
+btnpause.addEventListener("click", function () {
 	video.pause()
+	console.log("pause video")
 })
 
-btnslow.addEventListener("click",function(){
-	var speed_origin=video.playbackRate
-	if(speed_origin==0){
-		video.playbackRate=0
-	}
-	else{
-		video.playbackRate=speed_origin*(1-0.05)
-	}
+btnslow.addEventListener("click", function () {
+	video.playbackRate = video.playbackRate * (1 - 0.05)
 	console.log(video.playbackRate)
 })
 
-btnfast.addEventListener("click",function(){
-	var speed_origin=video.playbackRate
-	if(speed_origin==0){
-		video.playbackRate=0
-	}
-	else{
-		video.playbackRate=speed_origin/(1-0.05)
-	}
+btnfast.addEventListener("click", function () {
+	video.playbackRate = video.playbackRate / (1 - 0.05)
 	console.log(video.playbackRate)
 })
 
-btnskip.addEventListener("click",function(){
-	var skip_time=video.currentTime+15
-	if(skip_time<video.duration){
-		video.currentTime=skip_time
-	}
-	else if(skip_time>=video.currentTime){
-		video.currentTime=video.duration
+btnskip.addEventListener("click", function () {
+	var skip_time = video.currentTime + 15
+	if (skip_time < video.duration) {
+		video.currentTime = skip_time
+	} else if (skip_time >= video.currentTime) {
+		video.currentTime = video.duration
 	}
 	video.play()
 })
 
-btnmute.addEventListener("click",function(){
-	if(video.muted){
-		video.muted=false
-		this.innerHTML="mute"
-	}
-	else if (!video.muted){
-		video.muted=true
-		this.innerHTML="unmute"
+btnmute.addEventListener("click", function () {
+	if (video.muted) {
+		video.muted = false
+		this.innerHTML = "Mute"
+		slider.value=video.volume*100
+		volume.innerText=video.volume*100+"%"
+	} else if (!video.muted) {
+		video.muted = true
+		this.innerHTML = "Unmute"
+		slider.value=0
+		volume.innerText=0
 	}
 })
 
-
+slider.addEventListener("mouseup", function () {
+	volume.innerText = slider.value + "%"
+	video.volume = slider.value / 100
+	if (slider.value == 0) {
+		btnmute.innerHTML = "Unmute"
+	}else{
+		btnmute.innerHTML = "Mute"
+	}
+})
 
 // document.querySelector("#play").addEventListener("click", function() {
 // 	console.log("Play Video");
 // });
-
